279a5fe56cdd4e22c322c07bc4e834407a91a1f1
Kenneth Guerrero <kenetguerrero@gmail.com>
2020-07-04 20:09:33 +0800

    added google analytics.

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,6 @@
 import Header from "./header";
 import Footer from "./footer";
+import Head from "next/head";
 
 const layoutStyle = {
     margin: 0,
@@ -8,6 +9,24 @@ const layoutStyle = {
 
 const Layout = props => (
     <div style={layoutStyle}>
+
+        <Head>
+            <script
+                async
+                src="https://www.googletagmanager.com/gtag/js?id=[GA-TRACKING-ID]"
+            />
+            <script
+                dangerouslySetInnerHTML={{
+                __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', '[GA-TRACKING-ID]');
+                    `,
+                }}
+            />
+        </Head>
+
         <Header />
         {props.children}
         <Footer />
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Head from "next/head";
 import Layout from '../components/layout'
 
 export default function Home() {
